81356b9a75d919b79a75e9c6e78a773535f7b98d
jasonBirchall <jason.birchall@digital.justice.gov.uk>
2018-09-25 13:58:55 +0100

    Add terraform readme

---
+++ terraform/global-resources/s3.tf
@@ -0,0 +1,67 @@
+resource "aws_s3_bucket" "platform_terraform" {
+  bucket = "${var.project_name}-platform-terraform"
+  acl    = "private"
+
+  versioning {
+    enabled = true
+  }
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+}
+
+resource "aws_s3_bucket" "kops_state_store" {
+  bucket = "${var.project_name}-kops"
+  acl    = "private"
+
+  versioning {
+    enabled = true
+  }
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+}
+
+resource "aws_s3_bucket" "concourse_terraform" {
+  bucket = "${var.project_name}-concourse-terraform"
+  acl    = "private"
+
+  versioning {
+    enabled = true
+  }
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+}
+
+resource "aws_s3_bucket" "environments_terraform" {
+  bucket = "${var.project_name}-environments-terraform"
+  acl    = "private"
+
+  versioning {
+    enabled = true
+  }
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+}
