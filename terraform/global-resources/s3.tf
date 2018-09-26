f0e6bfcab46782a5e6e63f96ba711aeaf0ed03d1
jasonBirchall <jason.birchall@digital.justice.gov.uk>
2018-09-26 16:05:58 +0100

    Add global-resources Terraform for the test-2 cluster

---
+++ terraform/global-resources/s3.tf
@@ -0,0 +1,67 @@
+resource "aws_s3_bucket" "kops_state_store" {
+  bucket = "${var.project_name}-kops"
+  acl    = "private"
+
+  versioning {
+    enabled = true
+  }
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+}
+
+resource "aws_s3_bucket" "bastion_auth_key_store" {
+  bucket = "${var.project_name}-bastion-auth-keys"
+  acl    = "private"
+
+  versioning {
+    enabled = true
+  }
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+}
+
+resource "aws_s3_bucket" "concourse_terraform" {
+  bucket = "${var.project_name}-concourse"
+  acl    = "private"
+
+  versioning {
+    enabled = true
+  }
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+}
+
+resource "aws_s3_bucket" "environments_terraform" {
+  bucket = "${var.project_name}-environments"
+  acl    = "private"
+
+  versioning {
+    enabled = true
+  }
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+}
