9d63aef3858bfdd08a50698f8d2e9834d4f76747
jasonBirchall <jason.birchall@digital.justice.gov.uk>
2018-09-26 16:17:08 +0100

    Add terraform cloud-platform workspaces
    This enables the creation of parent VPC etc

---
+++ terraform/cloud-platform/main.tf
@@ -0,0 +1,80 @@
+# Setup
+terraform {
+  backend "s3" {
+    bucket = "cp-test-2-terraform"
+    region = "eu-west-1"
+    key    = "terraform-test-2.tfstate"
+  }
+}
+
+provider "aws" {
+  region = "eu-west-1"
+}
+
+data "terraform_remote_state" "global" {
+  backend = "s3"
+
+  config {
+    bucket = "cp-test-2-terraform"
+    region = "eu-west-1"
+    key    = "terraform.tfstate"
+  }
+}
+
+locals {
+  cluster_name             = "${terraform.workspace}"
+  cluster_base_domain_name = "${local.cluster_name}.k8s.cloud-platform"
+}
+
+# Modules
+module "cluster_dns" {
+  source                   = "../modules/cluster_dns"
+  cluster_base_domain_name = "${local.cluster_base_domain_name}"
+  parent_zone_id           = "${data.terraform_remote_state.global.k8s_zone_id}"
+}
+
+module "cluster_ssl" {
+  source                   = "../modules/cluster_ssl"
+  cluster_base_domain_name = "${local.cluster_base_domain_name}"
+  dns_zone_id              = "${module.cluster_dns.cluster_dns_zone_id}"
+}
+
+module "cluster_vpc" {
+  source               = "terraform-aws-modules/vpc/aws"
+  name                 = "${local.cluster_name}"
+  cidr                 = "${var.vpc_cidr}"
+  azs                  = "${var.availability_zones}"
+  private_subnets      = "${var.internal_subnets}"
+  public_subnets       = "${var.external_subnets}"
+  enable_nat_gateway   = true
+  enable_vpn_gateway   = false
+  enable_dns_hostnames = true
+
+  public_subnet_tags = {
+    SubnetType                                                = "Utility"
+    "kubernetes.io/cluster/${local.cluster_base_domain_name}" = "shared"
+    "kubernetes.io/role/elb"                                  = "1"
+  }
+
+  private_subnet_tags = {
+    SubnetType                                                = "Private"
+    "kubernetes.io/cluster/${local.cluster_base_domain_name}" = "shared"
+    "kubernetes.io/role/internal-elb"                         = "1"
+  }
+
+  tags = {
+    Terraform = "true"
+    Cluster   = "${local.cluster_name}"
+    Domain    = "${local.cluster_base_domain_name}"
+  }
+}
+
+resource "tls_private_key" "cluster" {
+  algorithm = "RSA"
+  rsa_bits  = "2048"
+}
+
+resource "aws_key_pair" "cluster" {
+  key_name   = "${local.cluster_base_domain_name}"
+  public_key = "${tls_private_key.cluster.public_key_openssh}"
+}
